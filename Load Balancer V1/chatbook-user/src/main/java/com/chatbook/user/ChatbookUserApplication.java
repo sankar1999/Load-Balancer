0c4cb744d89b3d347752348d25633eeeb081e0a7
sankar1999 <sankarsathya1973@gmail.com>
2022-12-02 02:11:22 +0530

    commit

--- Load Balancer V1/chatbook-user/src/main/java/com/chatbook/user/ChatbookUserApplication.java
+++ Load Balancer V1/chatbook-user/src/main/java/com/chatbook/user/ChatbookUserApplication.java
@@ -1,13 +1,37 @@
 package com.chatbook.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
+@RibbonClient(name = "chatbook", configuration = RibbonConfiguration.class)
 public class ChatbookUserApplication {
+
+	@Autowired
+	private RestTemplate template;
+
+	@GetMapping("/invoke")
+	public String invokeChatbook() {
+		String url = "http://chatbook/chatbook/chat";
+		return template.getForObject(url, String.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatbookUserApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
---
+++ Load Balancer V1/chatbook-user/src/main/java/com/chatbook/user/RibbonConfiguration.java
@@ -0,0 +1,25 @@
+package com.chatbook.user;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AvailabilityFilteringRule;
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.PingUrl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
+public class RibbonConfiguration {
+
+    @Autowired
+    public IClientConfig ribbonClient;
+
+    @Bean
+    public IPing ping(IClientConfig ribbonClient) {
+        return new PingUrl();
+    }
+
+    @Bean
+    public IRule rule(IClientConfig ribbonClient) {
+        return new AvailabilityFilteringRule();
+    }
+}
